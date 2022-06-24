4e2952be4d0f38aa61203e5b152c80c10ed74877
pablormacia <pablo-macia@live.com.ar>
2022-06-24 10:17:22 -0300

    Sistema empleados hasta prueba bd

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask 
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'sistema22090'
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+
+    sql = "INSERT INTO `empleados`(`id`, `nombre`, `correo`, `foto`) VALUES (NULL,'Persona','persona@mail.com','persona.jpg')"
+    conn = mysql.connect();
+    cursor = conn.cursor();
+    cursor.execute(sql);
+    conn.commit();
+    return render_template('empleados/index.html')
+
+@app.route('/juanca')
+def juancito():
+    return render_template('empleados/juanca.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
